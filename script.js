b78627551cb35dcb1f163210dc88b171c217bd0d
sebastianfrueh <sfrueh@gmx.net>
2022-05-23 22:19:50 +0200

    Add user input validation and event listeners for buttons

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
-const numberSquares = (16*16); 
-
 const container = document.querySelector('.container');
+const btnSize = document.querySelector('.btnSize');
+const btnReset = document.querySelector('.btnReset');
 
-//create divs depending on variable numberSquares
-for (let i = 0; i < numberSquares; i++) {
-    const div = document.createElement('div');
+//Add square creation with user input to button 
+btnSize.addEventListener('click', () => {
+    removeAllChildNodes(container);
+    createSquares(getUserInput());
+});
+
+//Add reset function to button 
+btnReset.addEventListener('click', () => {
+    document.querySelectorAll('.touched').forEach((touchedDiv) => {
+        touchedDiv.classList.remove('touched');
+    });
+});
+
+
+function createSquares(numberSquares) {
+    //set quantity of grid columns 
+    container.style.gridTemplateColumns = `repeat(${numberSquares}, 1fr)`;
+
+    //create divs depending on input
+    for (let i = 0; i < numberSquares * numberSquares; i++) {
+        const div = document.createElement('div');
     div.classList.add('square');
     div.addEventListener('mouseout', () => {
         div.classList.add('touched');
     }); 
     container.appendChild(div); 
+    };
 }
 
-// const squares = document.querySelectorAll('.square'); 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
-// squares.forEach(square => {
-//     square.addEventListener('mouseout', () => {
-//         square.classList.add('touched'); 
-//     });
-// }); 
+//limit for the user input
+function getUserInput() {
+    const userInput = parseInt(prompt('How much rows and columns?'));
+    if (userInput > 100) {
+        alert('Max allowed grid size is 100 x 100! Please choose again.');
+        return getUserInput();
+    };
+    return userInput;
+}
