613891f10e8c44814bbaa23ba14ba822f9adbb5a
sebastianfrueh <sfrueh@gmx.net>
2022-05-23 17:44:38 +0200

    Add event listener to divs to change color on touch

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const numberSquares = (16*16); 
 
-const container = document.querySelector('.container'); 
+const container = document.querySelector('.container');
 
-
+//create divs depending on variable numberSquares
 for (let i = 0; i < numberSquares; i++) {
-    const square = document.createElement('div');
-    square.classList.add('square');
-    container.appendChild(square); 
+    const div = document.createElement('div');
+    div.classList.add('square');
+    div.addEventListener('mouseout', () => {
+        div.classList.add('touched');
+    }); 
+    container.appendChild(div); 
 }
+
+// const squares = document.querySelectorAll('.square'); 
+
+// squares.forEach(square => {
+//     square.addEventListener('mouseout', () => {
+//         square.classList.add('touched'); 
+//     });
+// }); 
