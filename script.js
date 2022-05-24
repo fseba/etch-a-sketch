e7d635ed47b83cf390d86e97237e5b1901a0054c
sebastianfrueh <sfrueh@gmx.net>
2022-05-24 21:41:56 +0200

    Add check of switch status and change of square background to random color

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+let rainbowModeToggle = false; 
+
 const container = document.querySelector('.container');
 const btnSize = document.querySelector('.btnSize');
 const btnReset = document.querySelector('.btnReset');
+const btnSwitch = document.querySelector('.btnSwitch'); 
 
 //Add square creation with user input to button 
 btnSize.addEventListener('click', () => {
@@ -15,6 +18,10 @@ btnReset.addEventListener('click', () => {
     });
 });
 
+//Switch to activate rainbow mode
+btnSwitch.addEventListener('change', (e) => {
+    e.target.checked ? rainbowModeToggle = true : rainbowModeToggle = false;  
+});
 
 function createSquares(numberSquares) {
     //set quantity of grid columns 
@@ -23,9 +30,17 @@ function createSquares(numberSquares) {
     //create divs depending on input
     for (let i = 0; i < numberSquares * numberSquares; i++) {
         const div = document.createElement('div');
-    div.classList.add('square');
-    div.addEventListener('mouseout', () => {
-        div.classList.add('touched');
+        div.classList.add('square');
+        div.addEventListener('mouseout', () => {
+            if(rainbowModeToggle) {
+                if(!div.style.backgroundColor) {
+                div.style.backgroundColor = (`rgb(${Math. floor(Math. random() * 256)}, 
+                                                ${Math. floor(Math. random() * 256)}, 
+                                                ${Math. floor(Math. random() * 256)}, 0.8)`);
+                };
+            } else {
+                div.classList.add('touched');
+            };
     }); 
     container.appendChild(div); 
     };
@@ -39,9 +54,12 @@ function removeAllChildNodes(parent) {
 
 //limit for the user input
 function getUserInput() {
-    const userInput = parseInt(prompt('How much rows and columns?'));
+    const userInput = parseInt(prompt('How much rows and columns? Please enter a number from 1 and 100.'));
     if (userInput > 100) {
         alert('Max allowed grid size is 100 x 100! Please choose again.');
+        return getUserInput();
+    } else if (userInput < 1) {
+        alert('Please enter a number greater 0.');
         return getUserInput();
     };
     return userInput;
